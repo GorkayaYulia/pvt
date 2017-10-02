6cd05f06ff88574fff325e30cb078d288bf6472b
Pavel Rosolko <PavelRosolko@coherentsolutions.com>
2017-10-02 13:08:00 +0300

    structure init

---
+++ js/react intro/webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    context: __dirname,
+    entry: "./core",
+    output: {
+        path: './Build',
+        filename: 'bundle.js'
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+};
