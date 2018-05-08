b84306f9f375e7ac44d348201ad711c2f5d7d509
Pavel Rosolko <PavelRosolko@coherentsolutions.com>
2018-05-08 11:25:03 +0300

    update

---
+++ FD-01/lectures/12/README.md
@@ -0,0 +1,8 @@
+https://github.com/facebook/create-react-app
+
+npm i
+npx create-react-app my-app
+cd my-app
+npm start
+npm build
+npm test
---
+++ FD-01/lectures/12/components/helloWorld.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Hello extends Component {
+	render() {
+		return (
+			<div className="Hello">
+				Hello world
+			</div>
+		);
+	}
+}
+
+export default Hello;
